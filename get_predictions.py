8e3b056636cf1d6b8ad716588c17d8b6f4227c27
James Jiang <jamesjiang52@gmail.com>
2019-01-19 15:12:20 -0500

    Finish audio stream and classification

--- get_predictions.py
+++ get_predictions.py
@@ -1,17 +1,7 @@
-import os
 import requests
 
-
 url = "http://max-audio-classifier.max.us-south.containers.appdomain.cloud/model/predict"
-dir_ = "data/"
 
 
-def get_predictions():
-    files = os.listdir(dir_)
-    data = dict()
-
-    for filename in files:
-        best = requests.post(url=url, files={"audio": (dir_ + filename, open(dir_ + filename, "rb"))}).json()["predictions"][0]
-        data[dir_ + filename] = (best["label"], best["probability"])
-
-    return data
+def get_prediction(filename):
+    return [i["label"] for i in requests.post(url=url, files={"audio": (filename, open(filename, "rb"))}).json()["predictions"]]
