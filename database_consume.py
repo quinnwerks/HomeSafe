1d9ef95ff5365b5100832500fbeb8d94e9b887df
Quinn Smith <quinn.smith98@gmail.com>
2019-01-19 18:51:49 -0500

    added command parsing from mongodb

--- database_consume.py
+++ database_consume.py
@@ -1,10 +1,53 @@
 from pymongo import MongoClient
-
+from bson.json_util import dumps 
+import json
+import unicodedata
 client = MongoClient('mongodb://quinn2:quinn2@ds058508.mlab.com:58508/uofthacks')
 db=client.uofthacks
 posts = db.posts
 
 #post_data = posts.findOne()
+keyList = []
+valueList = []
+command = -1
 for document in posts.find():
-    print(document) # iterate the cursor
-posts.remove()
+    #sprint(document) # iterate the cursor
+    print document
+    #print type(document)
+    for key in document.keys():
+        print key
+        if key == "name":
+            #print key
+            keyList = keyList + [unicodedata.normalize('NFKD', document[key]).encode('ascii','ignore')]
+        elif key == "value":
+            #print key
+            valueList = valueList + [unicodedata.normalize('NFKD', document[key]).encode('ascii','ignore')]
+        else:
+            print "not name or value"
+
+if len(keyList) != len(valueList):
+    print 'error: kv length mismatch'
+
+
+if keyList[0] == 'Alarm_set':
+    if valueList[0] == 'On':
+        command = 1
+    elif valueList[0] == 'Off':
+        command = 2
+    else: 
+        print 'error: invalid Alarm_set value'
+
+print command
+
+
+
+print keyList
+print valueList
+#print type(keyList[0])
+#print(doc_json['name'])
+
+def dummy_function():
+    print 'do machine learning lmao'
+
+
+
--- database_produce.py
+++ database_produce.py
@@ -6,10 +6,71 @@ client = MongoClient('mongodb://quinn:quinn1@ds058508.mlab.com:58508/uofthacks')
 db=client.uofthacks
 posts = db.posts
 post_data = {
-    'title': 'Python and MongoDB',
-    'content': 'PyMongo is fun, you guys',
-    'author': 'Scott',
-    'daddy':'like'
+  "user": {
+    "userId": "ABwppHEZtWkz2oXVpIIzZXosjimP9aaIvsHNB2WmTLU3s6PxDKkBYBhsN46Q0TOCY_EZPhxo9ipU9j4gM_k",
+    "locale": "en-US",
+    "lastSeen": "2019-01-19T15:24:17Z"
+  },
+  "conversation": {
+    "conversationId": "ABwppHHsiGH5npIrOXr4_iO0MS8A3jcdzJYwYHAJGU1nt3ehNKSgzXDL_z8OPgzHF2t_EElcHnlJthu2uf8",
+    "type": "NEW"
+  },
+  "inputs": [
+    {
+      "intent": "enable",
+      "rawInputs": [
+        {
+          "inputType": "KEYBOARD",
+          "query": "talk to home safe turn on"
+        }
+      ],
+      "arguments": [
+        {
+          "name": "trigger_query",
+          "rawText": "turn on",
+          "textValue": "turn on"
+        },
+        {
+          "name": "enable",
+          "rawText": "turn on",
+          "textValue": "enable"
+        }
+      ]
+    }
+  ],
+  "surface": {
+    "capabilities": [
+      {
+        "name": "actions.capability.SCREEN_OUTPUT"
+      },
+      {
+        "name": "actions.capability.AUDIO_OUTPUT"
+      },
+      {
+        "name": "actions.capability.WEB_BROWSER"
+      },
+      {
+        "name": "actions.capability.MEDIA_RESPONSE_AUDIO"
+      }
+    ]
+  },
+  "isInSandbox": "true",
+  "availableSurfaces": [
+    {
+      "capabilities": [
+        {
+          "name": "actions.capability.SCREEN_OUTPUT"
+        },
+        {
+          "name": "actions.capability.AUDIO_OUTPUT"
+        },
+        {
+          "name": "actions.capability.WEB_BROWSER"
+        }
+      ]
+    }
+  ],
+  "requestType": "SIMULATOR"
 }
 # Issue the serverStatus command and print the results
 #serverStatusResult=db.command("serverStatus")
